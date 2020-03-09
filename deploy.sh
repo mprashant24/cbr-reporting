23c4e3e02a15c8f2cbd8d0a0c17877112ee0acef
Prashant Mishra <Prashant.Mishra@contrastsecurity.com>
2020-03-09 17:56:12 -0400

    updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -94,17 +94,17 @@ selectNodeVersion () {
   fi
 }
 
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Deployment
-:: ----------
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#:: Deployment
+#:: ----------
 
-:Deployment
+#:Deployment
 echo Handling node.js deployment.
 
-:: 1. Select node version
+#:: 1. Select node version
 call :SelectNodeVersion
 
-:: 2. Install npm packages
+#:: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
@@ -112,13 +112,13 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   popd
 )
 
-:: 3. Angular Prod Build
+#:: 3. Angular Prod Build
 IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
-:: If the above command fails comment above and uncomment below one
-:: call ./node_modules/.bin/ng build –prod
+#:: If the above command fails comment above and uncomment below one
+#:: call ./node_modules/.bin/ng build –prod
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
